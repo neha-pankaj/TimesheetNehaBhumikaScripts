7fd2f206a8eb5168f31f12ebe5962d63d1ed9e95
Neeraj <neeraj1wayitsol@gmail.com>
2020-01-03 14:22:08 +0530

    changes in base class ignore

--- src/timesheetauto/BaseClass.java
+++ src/timesheetauto/BaseClass.java
@@ -6,6 +6,7 @@ public class BaseClass {
 
 	@BeforeMethod
 	public void initialize() {
-	System.setProperty("webdriver.chrome.driver","C:\\Users\\Neeraj\\Desktop\\Neha workspace\\JetliTransfer\\chrome\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\neerajtesting\\git\\MyRepos\\Timesheet\\ChromeDriver2.36\\chromedriver.exe");
 	}
 }
