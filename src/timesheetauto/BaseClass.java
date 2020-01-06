65639e8fc62f8c0ccd4a57bafd68615fbdbbda4b
neha-pankaj <ntrehan92@gmail.com>
2020-01-06 13:17:08 +0530

    new changes regarding driver path

--- src/timesheetauto/BaseClass.java
+++ src/timesheetauto/BaseClass.java
@@ -7,6 +7,6 @@ public class BaseClass {
 	@BeforeMethod
 	public void initialize() {
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\neerajtesting\\git\\MyRepos\\Timesheet\\ChromeDriver2.36\\chromedriver.exe");
+				"C:\\Users\\Neeraj\\Desktop\\Neha workspace\\JetliTransfer\\chrome\\chromedriver.exe");
 	}
 }
